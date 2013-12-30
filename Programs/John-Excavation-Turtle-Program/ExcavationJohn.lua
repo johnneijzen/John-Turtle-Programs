4a0523397bbb6f8db1cc7d4dea50e2482a36e7e5
John Neijzen <John.neijzen@live.com>
2013-12-30 11:20:52 +0800

    Now Taps Varsion to read Normal

--- Programs/John-Excavation-Turtle-Program/ExcavationJohn.lua
+++ Programs/John-Excavation-Turtle-Program/ExcavationJohn.lua
@@ -1,7 +1,8 @@
 -- This Version
---  0.05
+--  0.06
 -- Changelogs
 --  0.05 - Minor bug fixing
+--  0.06 - Tabs Version
 
 -- Locals
 local wide = 0
@@ -20,126 +21,127 @@ local recheck = 0
 
 -- Checking
 function check()
-  if Fuelcount == 0 then
-    print("Turtle has no fuel")
-    Error = 1
-  end
-  if Fuelcount1 == 0 then
-    print("turtle has no extra fuel but if is short job it okey")
-  end
-  if Chest == 0 then
-    print("please put chests in 3 slot")
-    Error = 1
-  end
-  if Error = 1 then 
-    print("Items are missing please try again")
-    sleep(10)
-    recheck()
-  else 
-    print("all items are there turtle will start")
-    run()
-  end	
+	if Fuelcount == 0 then
+		print("Turtle has no fuel")
+		Error = 1
+	end
+	if Fuelcount1 == 0 then
+		print("turtle has no extra fuel but if is short job it okey")
+	end
+	if Chest == 0 then
+		print("please put chests in 3 slot")
+		Error = 1
+	end
+	if Error = 1 then 
+		print("Items are missing please try again")
+		sleep(10)
+		recheck()
+	else 
+		print("all items are there turtle will start")
+		run()
+	end	
 end
 
 -- Run Command
 function run()
-  turtle.digDown
-  turtle.down
-  turtle.digDown
-  turtle.down
-  turtle.digdown
-  WC = 0 
-  LC = 0
-  HC = HC + 3
-  if high == HC
-  long()
+	turtle.digDown
+	turtle.down
+	turtle.digDown
+	turtle.down
+	turtle.digdown
+	WC = 0 
+	LC = 0
+	HC = HC + 3
+	if high == HC
+		long()
+	end
 end
 
 -- Recheck  if user forget something turtle will check after 10 sec
 function recheck()
-  FuelCount = turtle.getItemCount(1)
-  FuelCount1 = turtle.getItemCount(2)
-  Chest = turtle.getItemCount(3)
-  Error = 0
-  check()
+	FuelCount = turtle.getItemCount(1)
+	FuelCount1 = turtle.getItemCount(2)
+	Chest = turtle.getItemCount(3)
+	Error = 0
+	check()
 end 
 
 -- Mining Long
 function long()
-  repeat
-    turtle.dig
-      if turtle.forward then
-	LC = LC + 1
-      end  
-     turtle.digUp
-     turtle.digDown
-     if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 5 to 16 will go to chest
-       turtle.select(3)
-       turtle.placeUp()
-       Chest = Chest - 1
-       for slot = 4, 16 do
-         turtle.select(slot)
-         turtle.dropUp()
-	 sleep(1) -- Small fix for slow pc because i had people problem with this
-       end
-     turtle.select(4)
-     end
-    repeat -- Fuel
-      if turtle.getFuelLevel() == "unlimited" then 
-        print("NO NEED FOR FUEL")
-        Needfuel = 0
-      elseif turtle.getFuelLevel() < 300 then
-        if FuelCount > 0 then
-	  turtle.select(1)
-          turtle.refuel(10)
-          Needfuel = 1
-          FuelCount = FuelCount - 10
-	elseif FuelCount1 > 0 then
-	  turtle.select(2)
-          turtle.refuel(10)
-          Needfuel = 1
-          FuelCount1 = FuelCount1 - 10
-	else
-	  print("out of fuel")
+	repeat
+		turtle.dig
+		if turtle.forward then
+			LC = LC + 1
+		end  
+		turtle.digUp
+		turtle.digDown
+		if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 5 to 16 will go to chest
+			turtle.select(3)
+			turtle.placeUp()
+			Chest = Chest - 1
+			for slot = 4, 16 do
+				turtle.select(slot)
+				turtle.dropUp()
+				sleep(1) -- Small fix for slow pc because i had people problem with this
+			end
+			turtle.select(4)
+		end
+		repeat -- Fuel
+			if turtle.getFuelLevel() == "unlimited" then 
+				print("NO NEED FOR FUEL")
+				Needfuel = 0
+			elseif turtle.getFuelLevel() < 300 then
+				if FuelCount > 0 then
+					turtle.select(1)
+					turtle.refuel(10)
+					Needfuel = 1
+					FuelCount = FuelCount - 10
+				elseif FuelCount1 > 0 then
+					turtle.select(2)
+					turtle.refuel(10)
+					Needfuel = 1
+					FuelCount1 = FuelCount1 - 10
+				else
+					print("out of fuel")
+				end
+			elseif NeedFuel == 1 then
+				Needfuel = 0
+			end
+		until NeedFuel == 0
+	until long = LC
+	if wide = WC then
+		run()
+	else 
+		wide()
 	end
-      elseif NeedFuel == 1 then
-        Needfuel = 0
-      end
-    until NeedFuel == 0
-  until long = LC
-  if wide = WC then
-    run()
-  else 
-    wide()
-  end
 end
 
 -- Mining Wide
 function wide()
-  if LSorWS = 0 then
-    turtle.turnRight()
-    turtle.dig()
-    turtle.forward()
-    turtle.digUp()
-    turtle.digDown()
-    turtle.turnRight()
-    LSorWS = 1
-  else 
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.forward()
-    turtle.digUp()
-    turtle.digDown()
-    turtle.turnRight()
-    LSorWS = 0
-  end
-  LC = 0
-  WC = WC + 1
-  if Wide == WC then
-    Run()
-  else
-    Long()
-  end
+	if LSorWS = 0 then
+		turtle.turnRight()
+		turtle.dig()
+		turtle.forward()
+		turtle.digUp()
+		turtle.digDown()
+		turtle.turnRight()
+		LSorWS = 1
+	else 
+		turtle.turnLeft()
+		turtle.dig()
+		turtle.forward()
+		turtle.digUp()
+		turtle.digDown()
+		turtle.turnRight()
+		LSorWS = 0
+	end
+	LC = 0
+	WC = WC + 1
+	if Wide == WC then
+		Run()
+	else
+		Long()
+	end
 end
 
 -- Starting
