07168d0105ab17f4e74536754db3000857d3cf15
John Neijzen <John.neijzen@live.com>
2014-03-02 13:38:41 +0800

    Update ExcavationJohn(No-Tabs).lua

--- Programs/John-Excavation-Turtle-Program/ExcavationJohn(No-Tabs).lua
+++ Programs/John-Excavation-Turtle-Program/ExcavationJohn(No-Tabs).lua
@@ -1,7 +1,8 @@
 -- This Version
---  0.10
+--  0.11
 -- Changelogs
 --  0.10 Rewrite
+--  0.11 Minor Bug Fixing and Change spacing
 
 -- local
 local Wide = 0
@@ -22,24 +23,24 @@ local Recheck = 0
 function check()
   if FuelCount == 0 then
     print("Turtle has no fuel")
-		print("put fuel in 1 first and second slot")
+    print("put fuel in 1 first and second slot")
     Error = 1
   else
-	  print("turtle has Fuel")
+    print("turtle has Fuel")
   end
   if FuelCount1 == 0 then
     print("turtle has no extra fuel but if is short job it okey")
   end
   if Chest == 0 then
     print("No chest in Turtle")
-		print("Put chest in 3 slot")
+    print("Put chest in 3 slot")
     Error = 1
-	else
-	  print("turtle has chest")
+  else
+    print("turtle has chest")
   end
   if Error == 1 then 
     print("Items are missing please try again")
-		print("turtle will recheck in 15 sec")
+    print("turtle will recheck in 15 sec")
     sleep(15)
     recheck()
   else 
@@ -97,17 +98,17 @@ function long()
     repeat -- Fuel
       if turtle.getFuelLevel() == "unlimited" then 
         print("NO NEED FOR FUEL")
-        Needfuel = 0
+        NeedFuel = 0
       elseif turtle.getFuelLevel() < 300 then
         if FuelCount > 10 then
           turtle.select(1)
           turtle.refuel(10)
-          Needfuel = 1
+          NeedFuel = 1
           FuelCount = FuelCount - 10
         elseif FuelCount1 > 10 then
           turtle.select(2)
           turtle.refuel(10)
-          Needfuel = 1
+          NeedFuel = 1
           FuelCount1 = FuelCount1 - 10
         else
           print("out of fuel")
@@ -143,8 +144,8 @@ function wide()
     turtle.turnRight()
     LSorWS = 0
   end
-  Lc = 0
-  Wc = Wc + 1
+    Lc = 0
+    Wc = Wc + 1
   if Wide == WC then
     run()
   else
