4ae71b23e4488e479ec3479f3ee426b6ba05d1e8
John Neijzen <John.neijzen@live.com>
2014-03-02 13:22:50 +0800

    Update ExcavationJohn(No-Tabs).lua

--- Programs/John-Excavation-Turtle-Program/ExcavationJohn(No-Tabs).lua
+++ Programs/John-Excavation-Turtle-Program/ExcavationJohn(No-Tabs).lua
@@ -1,39 +1,46 @@
 -- This Version
---  0.08
+--  0.10
 -- Changelogs
---  0.08 - Too much error from beta testing
+--  0.10 Rewrite
 
--- Locals
-local wide = 0
-local WC = 0
-local long = 0
-local LC = 0
-local high = 0
-local HC = 0
+-- local
+local Wide = 0
+local Wc = 0
+local Long = 0
+local Lc = 0
+local High = 0
+local Hc = 0
 local FuelCount = turtle.getItemCount(1)
 local FuelCount1 = turtle.getItemCount(2)
 local Chest = turtle.getItemCount(3)
-local Totalblocks = 0
+local TotalBlocks = 0
 local LSorWS = 0
 local Error = 0
-local recheck = 0
+local Recheck = 0
 
 -- Checking
 function check()
-  if Fuelcount == 0 then
+  if FuelCount == 0 then
     print("Turtle has no fuel")
+		print("put fuel in 1 first and second slot")
     Error = 1
+  else
+	  print("turtle has Fuel")
   end
-  if Fuelcount1 == 0 then
+  if FuelCount1 == 0 then
     print("turtle has no extra fuel but if is short job it okey")
   end
   if Chest == 0 then
-    print("please put chests in 3 slot")
+    print("No chest in Turtle")
+		print("Put chest in 3 slot")
     Error = 1
+	else
+	  print("turtle has chest")
   end
   if Error == 1 then 
     print("Items are missing please try again")
-    sleep(10)
+		print("turtle will recheck in 15 sec")
+    sleep(15)
     recheck()
   else 
     print("all items are there turtle will start")
@@ -41,24 +48,7 @@ function check()
   end 
 end
 
--- Run Command
-function run()
-  turtle.digDown()
-  turtle.down()
-  turtle.digDown()
-  turtle.down()
-  turtle.digDown()
-  WC = 0 
-  LC = 0
-  HC = HC + 3
-  if high == HC then
-    print("done")
-  else
-    long1()
-  end
-end
-
--- Recheck  if user forget something turtle will check after 10 sec
+-- Recheck  if user forget something turtle will check after 15 sec
 function recheck()
   FuelCount = turtle.getItemCount(1)
   FuelCount1 = turtle.getItemCount(2)
@@ -67,23 +57,40 @@ function recheck()
   check()
 end 
 
--- Mining Long
-function long1()
+-- Run Command
+function run()
+  turtle.digDown()
+  turtle.down()
+  turtle.digDown()
+  turtle.down()
+  turtle.digDown()
+  Wc = 0 
+  Lc = 0
+  Hc = Hc + 3
+  if High == Hc then
+    print("done")
+  else
+    long()
+  end
+end
+
+-- Mining for length
+function long()
   repeat
     turtle.dig()
-    if turtle.forward then
-      LC = LC + 1
+    if turtle.forward() then
+      Lc = Lc + 1
     end  
     turtle.digUp()
     turtle.digDown()
-    if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 5 to 16 will go to chest
+    if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 4 to 16 will go to chest
       turtle.select(3)
       turtle.placeUp()
       Chest = Chest - 1
       for slot = 4, 16 do
         turtle.select(slot)
         turtle.dropUp()
-        sleep(1) -- Small fix for slow pc because i had people problem with this
+        sleep(1.5) -- Small fix for slow pc because i had people problem with this
       end
       turtle.select(4)
     end
@@ -92,12 +99,12 @@ function long1()
         print("NO NEED FOR FUEL")
         Needfuel = 0
       elseif turtle.getFuelLevel() < 300 then
-        if FuelCount > 0 then
+        if FuelCount > 10 then
           turtle.select(1)
           turtle.refuel(10)
           Needfuel = 1
           FuelCount = FuelCount - 10
-        elseif FuelCount1 > 0 then
+        elseif FuelCount1 > 10 then
           turtle.select(2)
           turtle.refuel(10)
           Needfuel = 1
@@ -109,8 +116,8 @@ function long1()
         Needfuel = 0
       end
     until NeedFuel == 0
-  until long == LC
-  if wide == WC then
+  until Long == Lc
+  if Wide == Wc then
     run()
   else 
     wide()
@@ -136,12 +143,12 @@ function wide()
     turtle.turnRight()
     LSorWS = 0
   end
-  LC = 0
-  WC = WC + 1
+  Lc = 0
+  Wc = Wc + 1
   if Wide == WC then
-    Run()
+    run()
   else
-    long1()
+    long()
   end
 end
 
@@ -162,9 +169,13 @@ high = tonumber(input3)
 high = high - 5
 print("caluclating")
 Totalblocks = wide * long * high
-print("Total amount for block to mine")
+print("Total amount for block to mine is")
 print(Totalblocks)
 print("turtle now starting")
-turtle.select(1)
-turtle.refuel(4)
+if turtle.getFuelLevel() == "unlimited" then 
+  print("your turtle config does need fuel")
+elseif turtle.getFuelLevel() < 200 then
+  turtle.select(1)
+  turtle.refuel(2)
+end
 check()
