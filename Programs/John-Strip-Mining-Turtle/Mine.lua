c31597ef497f0edaba2563e3418c35522f1d10b5
John Neijzen <john.neijzen@live.com>
2014-10-05 00:17:06 +0800

    Update Mine.lua

--- Programs/John-Strip-Mining-Turtle/Mine.lua
+++ Programs/John-Strip-Mining-Turtle/Mine.lua
@@ -25,170 +25,170 @@ local Way = 0 -- 0 = Left and 1 = Right
 
 --Checking
 local function Check()
-  if torch == 0 then
-    print("There are no torch's in Turtle")
-    Error = 1
-  else
-    print("There are torch's in turtle")
-  end
-  if chest == 0 then
-    print("there are no chests")
-    Error = 1
-  else
-    print("There are chest in turtle")
-  end
-  if ItemFuel == 0 then
-    print("No Fuel Items")
-    error = 1
-  else
-    print("there is fuel")
-  end
-  repeat
-    if turtle.getFuelLevel() == "unlimited" then 
-      print("NO NEED FOR FUEL")
-      Needfuel = 0
-    elseif turtle.getFuelLevel() < 100 then
-      turtle.select(3)
-      turtle.refuel(1)
-      Needfuel = 1
-      ItemFuel = ItemFuel - 1
-    elseif NeedFuel == 1 then
-      Needfuel = 0
-    end
-  until NeedFuel == 0
+	if torch == 0 then
+		print("There are no torch's in Turtle")
+		Error = 1
+	else
+		print("There are torch's in turtle")
+	end
+	if chest == 0 then
+		print("there are no chests")
+		Error = 1
+	else
+		print("There are chest in turtle")
+	end
+	if ItemFuel == 0 then
+		print("No Fuel Items")
+		error = 1
+	else
+		print("there is fuel")
+	end
+	repeat
+		if turtle.getFuelLevel() == "unlimited" then 
+			print("NO NEED FOR FUEL")
+			Needfuel = 0
+		elseif turtle.getFuelLevel() < 100 then
+			turtle.select(3)
+			turtle.refuel(1)
+			Needfuel = 1
+			ItemFuel = ItemFuel - 1
+		elseif NeedFuel == 1 then
+			Needfuel = 0
+		end
+	until NeedFuel == 0
 end
 
 -- Recheck if user forget something turtle will check after 15 sec
 local function Recheck()
-  torch = turtle.getItemCount(1)
-  chest = turtle.getItemCount(2)
-  ItemFuel = turtle.getItemCount(3)
-  Error = 0
+	torch = turtle.getItemCount(1)
+	chest = turtle.getItemCount(2)
+	ItemFuel = turtle.getItemCount(3)
+	Error = 0
 end
 
 --Mining
 local function ForwardM()
-  repeat
-    if turtle.detect() then
-      turtle.dig()
-    end
-    if turtle.forward() then -- sometimes sand and gravel and block and mix-up distance
-      TF = TF - 1
-      onlight = onlight + 1
-    end
-    if turtle.detectUp() then
-      turtle.digUp()
-    end
-    turtle.select(4)
-    turtle.placeDown()
-    if onlight == 8 then -- Every 10 Block turtle place torch
-      turtle.turnLeft()
-      turtle.turnLeft()
-      turtle.select(1)
-      turtle.place()
-      turtle.turnLeft()
-      turtle.turnLeft()
-      torch = torch - 1
-      onlight = onlight - 8
-    end
-    if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 5 to 16 will go to chest
-      turtle.select(2)
-      turtle.digDown()
-      turtle.placeDown()
-      chest = chest - 1
-      for slot = 5, 16 do
-        turtle.select(slot)
-        turtle.dropDown()
-        sleep(1.5)
-      end
-      turtle.select(5)
-    end
-    repeat
-      if turtle.getFuelLevel() == "unlimited" then 
-        print("NO NEED FOR FUEL")
-        Needfuel = 0
-      elseif turtle.getFuelLevel() < 100 then
-        turtle.select(3)
-        turtle.refuel(1)
-        Needfuel = 1
-        ItemFuel = ItemFuel - 1
-      elseif NeedFuel == 1 then
-        Needfuel = 0
-      end
-    until NeedFuel == 0
-  until TF == 0
+	repeat
+		if turtle.detect() then
+			turtle.dig()
+		end
+		if turtle.forward() then -- sometimes sand and gravel and block and mix-up distance
+			TF = TF - 1
+			onlight = onlight + 1
+		end
+		if turtle.detectUp() then
+			turtle.digUp()
+		end
+		turtle.select(4)
+		turtle.placeDown()
+		if onlight == 8 then -- Every 10 Block turtle place torch
+			turtle.turnLeft()
+			turtle.turnLeft()
+			turtle.select(1)
+			turtle.place()
+			turtle.turnLeft()
+			turtle.turnLeft()
+			torch = torch - 1
+			onlight = onlight - 8
+		end
+		if turtle.getItemCount(16)>0 then -- If slot 16 in turtle has item slot 5 to 16 will go to chest
+			turtle.select(2)
+			turtle.digDown()
+			turtle.placeDown()
+			chest = chest - 1
+			for slot = 5, 16 do
+				turtle.select(slot)
+				turtle.dropDown()
+				sleep(1.5)
+			end
+		turtle.select(5)
+		end
+		repeat
+			if turtle.getFuelLevel() == "unlimited" then 
+				print("NO NEED FOR FUEL")
+				Needfuel = 0
+			elseif turtle.getFuelLevel() < 100 then
+				turtle.select(3)
+				turtle.refuel(1)
+				Needfuel = 1
+				ItemFuel = ItemFuel - 1
+			elseif NeedFuel == 1 then
+				Needfuel = 0
+			end
+		until NeedFuel == 0
+	until TF == 0
 end
 
 --Warm Up For Back Program
 local function WarmUpForBackProgram() -- To make turn around so it can go back
-  turtle.turnLeft()
-  turtle.turnLeft()
-  turtle.up()
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.up()
 end
 
 --Back Program
 local function Back()
-  repeat
-    if turtle.forward() then -- sometimes sand and gravel and block and mix-up distance
-      TB = TB - 1
-    end
-    if turtle.detect() then -- Sometimes sand and gravel can happen and this will fix it
-      if TB ~= 0 then
-        turtle.dig()
-      end
-    end
-  until TB == 0
+	repeat
+		if turtle.forward() then -- sometimes sand and gravel and block and mix-up distance
+			TB = TB - 1
+		end
+		if turtle.detect() then -- Sometimes sand and gravel can happen and this will fix it
+			if TB ~= 0 then
+				turtle.dig()
+			end
+		end
+	until TB == 0
 end
 
 -- Multimines Program
 local function MultiMines()
-  if Way == 0 then
-    turtle.turnLeft()
-    turtle.down()
-  else
-    turtle.turnRight()
-    turtle.down()
-  end
-  repeat
-    if turtle.detect() then
-      turtle.dig()
-    end
-    if turtle.forward() then
-      MD = MD - 1
-    end
-    if turtle.detectUp() then
-      turtle.digUp()
-    end
-  until MD == 0
-  if Way == 0 then
-    turtle.turnLeft()
-  else
-    turtle.turnRight()
-  end
-  if MineTimes == 0 then
-    print("Turtle is done")
-  else
-    MineTimes = MineTimes - 1
-  end
+	if Way == 0 then
+		turtle.turnLeft()
+		turtle.down()
+	else
+		turtle.turnRight()
+		turtle.down()
+	end
+	repeat
+		if turtle.detect() then
+			turtle.dig()
+		end
+		if turtle.forward() then
+			MD = MD - 1
+		end
+		if turtle.detectUp() then
+			turtle.digUp()
+		end
+	until MD == 0
+	if Way == 0 then
+		turtle.turnLeft()
+	else
+		turtle.turnRight()
+	end
+	if MineTimes == 0 then
+		print("Turtle is done")
+	else
+		MineTimes = MineTimes - 1
+	end
 end
 
 -- Restart 
 local function Restart()
-  TF = distance
-  TB = distance
-  MD = 3
-  onlight = 0
+	TF = distance
+	TB = distance
+	MD = 3
+	onlight = 0
 end
 
 -- Starting 
 function Start()
-  repeat
-    ForwardM()
-	WarmUpForBackProgram()
-	Back()
-	MultiMines()
-	Restart()
-  until MineTimes == 0
+	repeat
+		ForwardM()
+		WarmUpForBackProgram()
+		Back()
+		MultiMines()
+		Restart()
+	until MineTimes == 0
 end
 
 -- Start
@@ -207,10 +207,10 @@ input3 = io.read()
 MineTimes = tonumber(input3)
 Check()
 if Error == 1 then 
-  repeat
-    sleep(10)
-    Recheck()
-    Check()
-  until Error == 0
+	repeat
+		sleep(10)
+		Recheck()
+		Check()
+	until Error == 0
 end
 Start()
