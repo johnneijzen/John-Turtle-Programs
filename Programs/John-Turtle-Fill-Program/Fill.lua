21a5a35359eaedf5fc68963b64a9d30b86edb2d2
John Neijzen <john.neijzen@live.com>
2015-06-08 09:19:14 +0800

    Update Fill.lua

--- Programs/John-Turtle-Fill-Program/Fill.lua
+++ Programs/John-Turtle-Fill-Program/Fill.lua
@@ -5,9 +5,22 @@ local wide = 0
 local wideCount = 0
 local long = 0
 local longCount = 0
-local type = 1
+local selectType = 1
 local fuelItem = turtle.getItemCount(1)
 local buildSlot = 3
+
+local function refuel()
+  repeat
+    if turtle.getFuelLevel() < 100 then
+      if fuelItem > 0 then
+        turtle.select(1)
+        turtle.refuel(1)
+      else
+        os.shutdown()
+      end
+    end
+  until turtle.getFuelLevel () > 100 then
+end
 
 local function fill()
 repeat
@@ -30,3 +43,18 @@ repeat
   turtle.turnRight()
   wideCount = wideCount + 1
 until wide == wideCount
+
+--starting
+print("welcome to Fill Program")
+print("Please enter lenght you want to fill")
+input1 = io.read()
+long = tonumber(input1)
+print("Please Enter wide you want to fill")
+input2 = io.read()
+wide = tonumber(input2)
+print("Please enter what type fill you want")
+print("1 = Fill, 2 = Walls, 3 = Cube")
+input3 = io.read()
+selectType = tonumber(input3)
+fill()
+
