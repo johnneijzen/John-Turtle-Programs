47adc3a61b726817941aa528e7a10ecbcdbd27be
John Neijzen <john.neijzen@live.com>
2016-07-05 14:00:44 +0800

    Create Main.lua

---
+++ OOP/Main.lua
@@ -0,0 +1,12 @@
+local selector = 0;
+
+function Starting()
+{
+	print("Welcome John Turtle Programs Program")
+	print("list of programs")
+	if(selector = 0) then
+		print("--> Excavation Program")
+	else
+		print("    Excavation Program")
+	end
+}
