317877ec7af516c5978ed2e8b1e49ba4f80b4d0e
John Neijzen <John.neijzen@live.com>
2016-07-07 01:21:12 +0800

    More Updates and Layout Plans

---
+++ OOP/Updater.lua
@@ -0,0 +1,22 @@
+--[[
+	version:
+		0.01
+	Changelogs:
+		0.01 - Trying to make updater
+]]--
+
+local link = "https://raw.githubusercontent.com/johnneijzen/John-Turtle-Programs/OOP-format-John-2016-WIP%5D/OOP/Main.lua"
+local link2 = "https://raw.githubusercontent.com/johnneijzen/John-Turtle-Programs/OOP-format-John-2016-WIP%5D/OOP/JohnExcavator.lua"
+local link3 = "https://raw.githubusercontent.com/johnneijzen/John-Turtle-Programs/OOP-format-John-2016-WIP%5D/OOP/Fill(2016).lua"
+
+local function getList()
+	
+end
+
+local function main()
+	print("The Program Will Download All Files Need For John Turtle Programs")
+	prinf("Now Checking")
+	
+end
+
+main()
